7a89e157a41213203d824921c481cf4e7849fee4
jeremia-rantsho <rantshodiketso17@gmail.com>
2024-05-30 15:16:36 +0200

    changed from hard-coding to handlebars

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import handlebars from 'vite-plugin-handlebars';
 import data from './data.json';
 
 export default {
+  base : '/responsive_cv/',
   plugins: [
     handlebars({
-      context: data
+      context: data,
     }),
   ],
 };
